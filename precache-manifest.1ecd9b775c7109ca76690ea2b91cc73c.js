4e0445fb3da427b05ac2218c9363e3547f627915
dianingsps <dianingsukmapilia@gmail.com>
2019-04-29 13:46:28 +0700

    Updates

---
+++ precache-manifest.1ecd9b775c7109ca76690ea2b91cc73c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa742b3129d391d3b451e52fff7dbaad",
+    "url": "/latianweb3/index.html"
+  },
+  {
+    "revision": "ed5db6c032d80090c43b",
+    "url": "/latianweb3/static/css/2.dc646829.chunk.css"
+  },
+  {
+    "revision": "49bd90153296e4448dc3",
+    "url": "/latianweb3/static/css/main.71b0204b.chunk.css"
+  },
+  {
+    "revision": "ed5db6c032d80090c43b",
+    "url": "/latianweb3/static/js/2.8caf5d1a.chunk.js"
+  },
+  {
+    "revision": "49bd90153296e4448dc3",
+    "url": "/latianweb3/static/js/main.4dabbca4.chunk.js"
+  },
+  {
+    "revision": "67c505f865d40f79e9a3",
+    "url": "/latianweb3/static/js/runtime~main.f2beccc9.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/latianweb3/static/media/logo.ee7cd8ed.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 importScripts(
-  "/latianweb3/precache-manifest.1efa533baf1c08cbf89e254c80fdea0f.js"
+  "/latianweb3/precache-manifest.1ecd9b775c7109ca76690ea2b91cc73c.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/latianweb3/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/latianweb3/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
